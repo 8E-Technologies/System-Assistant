b44d4ffbdd2ce1e74d1e197b0f18dbafb7385b2e
Brian Leyva <55462869+xxSShadow@users.noreply.github.com>
2022-04-20 12:15:51 -0700

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,4 +1,4 @@
-const { Prefix, Token } = process.env;
+const { TOKEN, PREFIX } = process.env;
 
 const { Client, Intents, Collection } = require('discord.js');
 const client = new Client({ intents: new Intents(32767) });
@@ -13,51 +13,32 @@ for (File of Folder) {
   client.commands.set(Command.name, Command);
 }
 
-client.once('ready', async () => {
-  console.log(`${Client.user.username} Is Online!`);
+client.once('ready', () => {
+  console.log(`${client.user.username} Is Online!`);
 });
 
 client.on('message', async (message) => {
-  if (!message.content.startsWith(Prefix) || message.author.bot) {return}
-  Arguments = message.content.slice(Prefix.length).split(/ +/);
+  if (!message.content.startsWith(PREFIX) || message.author.bot) {return}
+  Arguments = message.content.slice(PREFIX.length).split(/ +/);
   Command = Arguments.shift();
   
-  if (Command === 'help') {
-    client.commands.get('Help').execute(message)
-  }
+  if (Command === 'help') { client.commands.get('Help').execute(message) }
   
-  if (Command === 'information') {
-    client.commands.get('Information').execute(message)
-  }
+  if (Command === 'information') { client.commands.get('Information').execute(message) }  
   
-  if (Command === 'protection') {
-    client.commands.get('Protection').execute(message)
-  }
+  if (Command === 'protection') { client.commands.get('Protection').execute(message) }
   
-  if (Command === 'settings') {
-    client.commands.get('Settings').execute(message)
-  }
+  if (Command === 'settings') { client.commands.get('Settings').execute(message) }
   
-  if (Command === 'ban') {
-    client.commands.get('Ban').execute(message)
-  }
+  if (Command === 'ban') { client.commands.get('Ban').execute(message) }
   
-  if (Command === 'kick') {
-    client.commands.get('Kick').execute(message)
-  }
+  if (Command === 'kick') { client.commands.get('Kick').execute(message) }
   
-  if (Command === 'mute') {
-    client.commands.get('Mute').execute(message)
-  }
+  if (Command === 'mute') { client.commands.get('Mute').execute(message) }
   
-  if (Command === 'respect') {
-    client.commands.get('Respect').execute(message)
-  }
+  if (Command === 'respect') { client.commands.get('Respect').execute(message) }
   
-  if (Command === 'ping') {
-    client.commands.get('Ping').execute(message)
-  }
-
+  if (Command === 'ping') { client.commands.get('Ping').execute(message) }
 });
 
-client.login(Token);
+client.login(TOKEN);
